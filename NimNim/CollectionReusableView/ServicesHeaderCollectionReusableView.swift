78f970a2e45919a076968f32ac4232cd96aed0ce
Raghav Vij <raghav@lbbd.in>
2019-10-12 09:57:44 +0530

    Test commit

--- NimNim/CollectionReusableView/ServicesHeaderCollectionReusableView.swift
+++ NimNim/CollectionReusableView/ServicesHeaderCollectionReusableView.swift
@@ -19,6 +19,7 @@ class ServicesHeaderCollectionReusableView: UICollectionReusableView {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        print("")
     }
     
     //MARK: IBActions
